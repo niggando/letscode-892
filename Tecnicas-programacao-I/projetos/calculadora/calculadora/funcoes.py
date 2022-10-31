805024d9e8d99af15d9c1a6c16cd6cf7d422e19f
Kaio <eng.kaio17@gmail.com>
2022-10-31 20:05:37 -0300

    Adicionando subtracao, divisao e multiplicacao

--- Tecnicas-programacao-I/projetos/calculadora/calculadora/funcoes.py
+++ Tecnicas-programacao-I/projetos/calculadora/calculadora/funcoes.py
@@ -12,3 +12,51 @@ def soma(a,b):
     
     else:
         raise TypeError(f"O input de 'a' e 'b' devem ser um número, recebido {a} ({type(a)}) e {b} ({type(b)})")
+    
+def subtracao(a,b):
+    """
+    Args:
+        a: (int, float)
+        b: (int, float)
+    
+    return:
+        a + b: (int, float)
+    """
+    if isinstance(a, (int, float)) or (b, (int, float)):
+        return a-b
+    
+    else:
+        raise TypeError(f"O input de 'a' e 'b' devem ser um número, recebido {a} ({type(a)}) e {b} ({type(b)})")
+
+def divisao(a,b):
+    """
+    Args:
+        a: (int, float)
+        b: (int, float)
+    
+    return:
+        a + b: (int, float)
+    """
+    if isinstance(a, (int, float)) or (b, (int, float)):
+        if a == 0 or b == 0:
+            return 0
+        else:
+            return a/b
+    
+    else:
+        raise TypeError(f"O input de 'a' e 'b' devem ser um número, recebido {a} ({type(a)}) e {b} ({type(b)})")
+
+def multiplicacao(a,b):
+    """
+    Args:
+        a: (int, float)
+        b: (int, float)
+    
+    return:
+        a + b: (int, float)
+    """
+    if isinstance(a, (int, float)) or (b, (int, float)):
+        return a*b
+    
+    else:
+        raise TypeError(f"O input de 'a' e 'b' devem ser um número, recebido {a} ({type(a)}) e {b} ({type(b)})")
